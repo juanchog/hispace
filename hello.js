99369d346d8cb7209a8ad8e725c559c5f42bef62
carlosbkm <carlos.garcia.mejias@gmail.com>
2015-04-11 12:05:36 +0200

    Pruebas de obtener latitud y longitud de la ISS

--- hello.js
+++ hello.js
@@ -1,15 +1,39 @@
 if (Meteor.isClient) {
+    Meteor.subscribe("getLat");
   Session.setDefault('twitPublished', false);
+    Session.setDefault('latitude', "latitude");
+    Session.setDefault('longitud', "longitud");
 
   Template.hello.helpers({
     twitPublished: function () {
       return Session.get('twitPublished');
-    }
+    },
+
   });
+    Template.issposition.helpers({
+        isslatitude: function () {
+            var latitude = Session.get('latitude');
+            console.log("latitude" + latitude);
+            return Session.get('latitude');
+        },
+        isslongitud: function () {
+            var longitud = Session.get('longitud');
+            console.log("longitud" + longitud);
+            return Session.get(longitud);
+        }
+    });
 
   Template.hello.events({
     'click button': function () {
-        Meteor.call('postTweet');
+        //Meteor.call('postTweet');
+        var position = Meteor.call('positionOfISS');
+        console.log("ogjwgoihwego");
+        console.log(position);
+        console.log(Session.get('miau'));
+        console.log("miau");
+        Session.set('latitude', position.iss_position.latitude);
+        Session.set('longitud', position.iss_position.longitude);
+        console.log("longitud" + longitud);
     }
   });
 }
@@ -27,12 +51,22 @@ if (Meteor.isServer) {
                 , access_token: '3154707879-qTsy7TjARsQePUfAbuQQzDAeRiUIKfgrvInenWQ'
                 , access_token_secret: 'SD4lqNETA8quP12U7ukSSGAPHTgoBVNZFRcBIFCnifd0o'
             });
-            Twit.post('statuses/update', {status: 'hello ISS 2!'}, function (err, data, response) {
-                Session.set('twitPublished', true);
+            Twit.post('statuses/update', {status: 'Test from Carlos'}, function (err, data, response) {
+
                 console.log(data);
                 console.log(reponse);
             });
+        },
+        positionOfISS: function() {
+            var res = Meteor.http.get("http://api.open-notify.org/iss-now.json");
+            console.log(res.data.iss_position.latitude, res.data.iss_position.longitude);
+            return res.data;
+
         }
     });
+    Meteor.publish("getLat", function() {
+        return "riaun";
+    });
+
 
 }
